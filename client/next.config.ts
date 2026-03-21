7f9bac716d6e2f5788210dcf912abc448dc48fee
Solomon Getnet <solomongetnet.dev@gmail.com>
2026-03-22 02:43:28 +0300

    Vercel issue fixed

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: { domains: ["res.cloudinary.com", "lh3.googleusercontent.com"] },
+  images: {
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Prevent Next.js from trying to bundle React Native async-storage
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "@react-native-async-storage/async-storage": false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
